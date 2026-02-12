890606d033ceae8d64d4e7947b440d4a9bec3f54
GR1ZL1_ <uraderevyanko2010@gmail.com>
2026-02-12 19:10:34 +0100

    fixed some issues and added default wallpapers

---
+++ wset.sh
@@ -0,0 +1,6 @@
+mkdir $HOME/Wallpapers
+cp -r $HOME/arch_hypr-dots/wallpapers/hyprlock $HOME/Wallpapers
+cp $HOME/arch_hypr-dots/wallpapers/default_bg.jpg $HOME/Wallpapers
+swww img $HOME/Wallpapers/default_bg.jpg
+
+echo "installation fully completed the directory(arch_hypr-dots) can be safely deleted"
