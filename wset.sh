60d3eff67d91d8a8c50f4de0fa8689af88c438dc
GR1ZL1_ <uraderevyanko2010@gmail.com>
2026-02-13 11:21:56 +0100

    Update wallpaper setup script and notifications
    Remove old exec-once.conf and notify user on completion.

--- wset.sh
+++ wset.sh
@@ -1,6 +1,8 @@
 mkdir $HOME/Wallpapers
+rm -rf $HOME/.config/hypr/hyprland/exec-once.conf
+cp $HOME/arch_hypr-dots/auto/auto1/exec-once.conf $HOME/.config/hypr/hyprland
 cp -r $HOME/arch_hypr-dots/wallpapers/hyprlock $HOME/Wallpapers
 cp $HOME/arch_hypr-dots/wallpapers/default_bg.jpg $HOME/Wallpapers
 swww img $HOME/Wallpapers/default_bg.jpg
 
-echo "installation fully completed the directory(arch_hypr-dots) can be safely deleted"
+notify-send -t 1500 "Dotfiles" "Installation is fully completed, u can safely delete $HOME/arch_hypr-dots yet"
