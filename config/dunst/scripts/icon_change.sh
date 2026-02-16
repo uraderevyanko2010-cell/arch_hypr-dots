616426fb2d5a11f9e2ced61903302765541beb41
GR1ZL1_ <uraderevyanko2010@gmail.com>
2026-02-16 18:18:40 +0100

    Delete config directory

--- config/dunst/scripts/icon_change.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-STATE=$(dunstctl is-paused)
-
-if [ "$STATE" = "true" ]; then
-    echo "󰂛"   # muted
-else
-    echo "󰂚"   # active
-fi
--- config/waybar/scripts/screenshot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/env bash
-
-grim -g "$(slurp)" "$HOME/Pictures/$(date +%F_%T)_area.png" 
-sleep 0.5
-notify-send "Waybar" "Screenshot saved to '~/Pictures'"
