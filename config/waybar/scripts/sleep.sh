59fb703230117724593491dc8b0450a4cae40b60
GR1ZL1_ <uraderevyanko2010@gmail.com>
2026-02-25 18:23:14 +0100

    Delete config directory

--- config/waybar/scripts/sleep.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-COUNT=$(pgrep -x hypridle | wc -l)
-
-if [ "$COUNT" -eq 0 ]; then
-    echo "󰒳"   # disabled / awake
-else
-    echo "󰒲"   # active
-fi
--- config/waybar/scripts/update-system.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-kitty --hold yay -Syu && pacman -Syu
